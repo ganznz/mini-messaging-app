8b182db9c303032d06c7226cb7462b4f99de6f71
ganznz <loganbellinghamlb@gmail.com>
2024-11-01 23:36:03 +1300

    requests to express server now reroute to 8080 by default

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+
+const env = loadEnv("all", process.cwd());
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    server: {
+        proxy: {
+            "/": {
+                target:
+                    env.VITE_ENV === "dev"
+                        ? env.VITE_APU_URL_DEV
+                        : env.VITE_APU_URL_PROD,
+                changeOrigin: true,
+            },
+        },
+    },
+});
