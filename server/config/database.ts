99697c5b3e07c87d1053a3d24afa6d94636c6451
ganznz <loganbellinghamlb@gmail.com>
2024-11-01 23:40:39 +1300

    initialized mongodb connection

--- server/config/database.ts
+++ server/config/database.ts
@@ -1,24 +1,17 @@
-import { config } from "dotenv";
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-config();
+export const connectDB = async () => {
+    const uri = `mongodb+srv://${process.env.MONGODB_CONNECTION_CREDENTIALS}@cluster0.e7dld.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+    const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        },
+    });
 
-const uri = `mongodb+srv://${process.env.MONGODB_CONNECTION_CREDENTIALS}@cluster0.e7dld.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    },
-});
-const run = async () => {
-    try {
-        await client.connect();
-        await client.db("admin").command({ ping: 1 });
-        console.log("Successfully connected to DB");
-    } finally {
-        // ensure client closes on finish/error
-        await client.close();
-    }
+    await client.connect();
+    console.log("Connected to DB");
+
+    return client;
 };
-run().catch(console.dir);
--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,34 @@
-// const express = require("express");
+import { config } from "dotenv";
+import { fileURLToPath } from "url";
+import path from "path";
+
 import express, { Request, Response } from "express";
-const PORT = 5000;
+import { connectDB } from "./config/database.js";
+
+const PORT = 8080;
+
+// get dir name of the current module
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+// configure dotenv w/ the explicit path
+// gotta do this because the cmd that executes this file isn't in same dir as this file
+config({ path: path.resolve(__dirname, ".env") });
 
 const app = express();
+const dbClient = await connectDB();
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Hello World");
+app.get("/", async (req: Request, res: Response) => {
+    try {
+        const db = dbClient.db("sample_mflix");
+        const users = db.collection("users");
+        const user = await users.findOne({ name: "Ned Stark" });
+        console.log(user);
+        res.send("*kissy emoji*");
+    } catch (err) {
+        console.error(err);
+    }
 });
+
+app.get("/new", (req: Request, res: Response) => {});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
