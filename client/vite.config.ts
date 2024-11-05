a5804a3971ae4928778964696355256eb0e91900
ganznz <loganbellinghamlb@gmail.com>
2024-11-05 23:24:03 +1300

    modified config files

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -15,6 +16,11 @@ export default defineConfig({
                         : env.VITE_APU_URL_PROD,
                 changeOrigin: true,
             },
+        },
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
         },
     },
 });
