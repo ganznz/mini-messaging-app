3d8cdf45afaac25e35756d2ce0313091ea1be2c3
ganznz <loganbellinghamlb@gmail.com>
2024-11-08 22:17:10 +1300

    created MessageDisplay component

---
+++ client/src/components/composite/MessageDisplay.tsx
@@ -0,0 +1,44 @@
+import { MessageCard } from "./MessageCard";
+
+import { useEffect, useState, useCallback } from "react";
+
+import { useMessages } from "@/hooks/useMessages";
+import { useNewMessageSocket } from "@/hooks/useNewMessageSocket";
+import { MessageParsed } from "../schemas/schemas";
+
+export const MessageDisplay = () => {
+    const { messages: initialMessages, loading, err } = useMessages();
+    const [messages, setMessages] = useState(initialMessages);
+
+    // set initial messages to messages state
+    useEffect(() => {
+        setMessages(initialMessages);
+    }, [initialMessages]);
+
+    // cache this function between re-renders
+    const handleNewMessage = useCallback((msg: MessageParsed) => {
+        setMessages((prev) => [msg, ...(prev || [])]);
+    }, []);
+
+    useNewMessageSocket(handleNewMessage);
+
+    return (
+        <div className="w-full h-full overflow-scroll p-4">
+            {loading && <p>Loading messages...</p>}
+            {err && <p>Error: {err.message}</p>}
+            {messages
+                ? messages.map(({ uid, user, text, added }) => {
+                      // message card
+                      return (
+                          <MessageCard
+                              key={uid}
+                              user={user}
+                              message={text}
+                              added={added}
+                          />
+                      );
+                  })
+                : "No messages..."}
+        </div>
+    );
+};
