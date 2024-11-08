fd81529967b8cd8b147667677b0950b8ecefa59e
ganznz <loganbellinghamlb@gmail.com>
2024-11-08 17:27:25 +1300

    updated server & client schemas

--- client/src/components/schemas/schemas.ts
+++ client/src/components/schemas/schemas.ts
@@ -1,10 +1,10 @@
 import { z } from "zod";
 
 export const formSchema = z.object({
-    username: z.string().min(3, { message: "Username is too short!" }).max(20, {
+    user: z.string().min(3, { message: "Username is too short!" }).max(20, {
         message: "Username is too long!",
     }),
-    msg: z
+    text: z
         .string()
         .min(10, { message: "Message is too short!" })
         .max(150, { message: "Message is too short!" }),
--- server/schema.ts
+++ server/schema.ts
@@ -1,5 +1,14 @@
+import { Request } from "express";
+
 export interface Message {
     user: string;
     text: string;
     added: Date;
+}
+
+export interface PostMessageRequest extends Request {
+    body: {
+        user: string;
+        text: string;
+    };
 }
