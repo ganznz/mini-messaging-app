081454abe70fa6df9afc9ac619ee7380852a4547
ganznz <loganbellinghamlb@gmail.com>
2024-11-08 22:27:30 +1300

    render navbar, message display & messageform

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,35 @@
+import { Navbar } from "@/components/composite/Navbar";
+import { MessageDisplay } from "@/components/composite/MessageDisplay";
+import { MessageForm } from "@/components/composite/MessageForm";
+import { Button } from "./components/ui/button";
+
+import { useState } from "react";
+
 function App() {
-    return <p>Hello</p>;
+    const [formOpen, setFormOpen] = useState(false);
+
+    return (
+        <>
+            {/* navbar */}
+            <Navbar className="p-4 flex-col items-center group sm:flex-row">
+                <h1 className="text-2xl font-bold sm:mr-auto">
+                    Mini Messaging App
+                </h1>
+                <Button
+                    className="text-lg bg-[hsl(var(--dark-grey))] hover:bg-[hsl(var(--green))] hover:text-[hsl(var(--green-foreground))] transition-all duration-200"
+                    onClick={() => setFormOpen(true)}
+                >
+                    Send Message
+                </Button>
+            </Navbar>
+
+            {/* message display */}
+            <MessageDisplay />
+
+            {/* message form */}
+            {formOpen && <MessageForm setFormOpen={setFormOpen} />}
+        </>
+    );
 }
 
 export default App;
